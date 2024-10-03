455c6db4ad1c6b49fce376bba683d653574947f7
VoQuocViet4323 <140177566+VoQuocViet4323@users.noreply.github.com>
2024-10-03 20:41:30 +0700

    add project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const int Scale = 23000;
+        Console.WriteLine($"Nhap so tien ma ban co");
+        int USD = int.Parse(Console.ReadLine()); 
+        int tien = Scale * USD;
+        Console.WriteLine($"So tien Viet ma ban co la: {tien}");
+        
+    }
+}
